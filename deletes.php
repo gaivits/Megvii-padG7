8f47b66c46aa10242543baaf04a83696f19397b1
gaivits <gaivit123@hotmail.com>
2021-06-25 16:15:14 +0700

    sent_URL_value

--- deletes.php
+++ deletes.php
@@ -2,11 +2,13 @@
 
 $curl = curl_init();
 $ck = $_COOKIE['cc'];
+
 $id = $_GET['id'];
-$per_id = ["person_list"=>["id"=>$id]];
+echo gettype($id);
+$per_id = ["person_list"=>["id"=>$_GET['id']]];
 $pf = json_encode($per_id);
 curl_setopt_array($curl, array(
-  CURLOPT_URL => '192.168.1.66/api/persons/item/${id}',
+  CURLOPT_URL => '192.168.1.66/api/persons/item/57',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -20,8 +22,7 @@ curl_setopt_array($curl, array(
 ));
 
 $response = curl_exec($curl);
-
 curl_close($curl);
 echo $response;
-echo "<script language='JavaScript'>alert('Deleted-OK');</script>";
+echo "<script language='JavaScript'>alert(' . $response . ');</script>";
 echo "<script language='JavaScript'>window.location.href='member.php';</script>";
