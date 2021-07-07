4f7cdbe69237ac318e344343ff28fc266268c041
gaivits <gaivit123@hotmail.com>
2021-07-07 17:43:04 +0700

    Some-files

--- member.php
+++ member.php
@@ -32,7 +32,7 @@
             $data .= "<td>" . $document['id']."</td>";
             $data .= "<td>" . $document['type']."</td>";
             $data .= "<td>" . $document['person_name']."</td>";
-            $data .= "<td>"."<a href=deletes.php?id=$document[id]>"."Del"."</td>";
+            $data .= "<td>"."<a onClick=\"javascript: return confirm('หากคุณลบออกประวัติการทำรายการจะหายไปด้วย ต้องการลบหรือไม่');\" href=deletes.php?id=$document[id]>"."Del"."</td>";
             $data .= "<td>"."<a href=edits.php?id=$document[_id]&ids=$document[id]>"."Edit"."</td>";
             $c++;
             $data .= "</tr>";
@@ -41,6 +41,7 @@
         $data .= "<td>" ."<b>".$c." ROW-AFFECTED "."</b>"."</td>";
         $data .= "</table>";
         echo $data;
+
 
         
 ?>
